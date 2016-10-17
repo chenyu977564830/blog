3ce2a80c8905fadf47d8fc28dc451c11fcbe6680
chenyu977564830 <977564830@qq.com>
2016-10-17 17:23:50 +0800

    验证码 用户登录已做好

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Http\Controllers\Admin;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Crypt;
+use App\Http\Model\User;
+require_once('resources/org/code/Code.class.php');
+class LoginController extends CommonController
+{
+    public function login()
+    {
+        //判断是否为post表单提交
+    	if(!$input=Input::all())return view('admin.login');
+		//检查验证码
+        if(strtoupper($input['code'])!=$_SESSION['code'])return back()->with('msg','验证码错误');//back函数将返回的变量存入了session中
+
+        $user=User::where('user_name',$input['user_name'])->first();
+        if(!$user)return back()->with('msg','用户名不存在');
+        $user_pass=Crypt::decrypt($user->user_pass);
+        if($user_pass!=$input['user_pass'])return back()->with('msg','密码错误');
+
+        echo 'ok';
+    }
+
+    public function code(){
+    	$code=new \Code();
+    	$code->make();
+    }
+ 
+}
